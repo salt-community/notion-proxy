e5174c6f84e0e8c6ac9256eefa3722ea4f2fedea
Kiran Malieakkal <kiranmalieakkal18@gmail.com>
2024-09-03 15:51:55 +0200

    Setting up the SpringBoot Project

---
+++ src/main/java/com/saltpgp/notionproxy/NotionproxyApplication.java
@@ -0,0 +1,13 @@
+package com.saltpgp.notionproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotionproxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotionproxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saltpgp/notionproxy/NotionproxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saltpgp.notionproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotionproxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
