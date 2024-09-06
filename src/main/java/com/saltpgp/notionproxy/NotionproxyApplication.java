8950cc5808d8c225a42536b52697162ba0e43666
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-09-06 09:18:41 +0200

    Ignore auto generated password
    Co-authored-by: Emiliana <emilianalila@gmail.com>
    Co-authored-by: Kiran Malieakkal <kiranmalieakkal18@gmail.com>
    Co-authored-by: Liam Frosteman Neogard <liamfrostemanneogard@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/main/java/com/saltpgp/notionproxy/NotionproxyApplication.java
+++ src/main/java/com/saltpgp/notionproxy/NotionproxyApplication.java
@@ -2,8 +2,9 @@ package com.saltpgp.notionproxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class NotionproxyApplication {
 
 	public static void main(String[] args) {
