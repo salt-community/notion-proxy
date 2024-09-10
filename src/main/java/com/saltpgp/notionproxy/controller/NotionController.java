75cc4de5b67ed1da5c8a7e386262cb2ffca9a4e8
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-09-10 10:24:45 +0200

    Return DTOs instead of models
    Co-authored-by: Emiliana <emilianalila@gmail.com>
    Co-authored-by: Kiran Malieakkal <kiranmalieakkal18@gmail.com>
    Co-authored-by: Liam Frosteman Neogard <liamfrostemanneogard@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/main/java/com/saltpgp/notionproxy/controller/NotionController.java
+++ src/main/java/com/saltpgp/notionproxy/controller/NotionController.java
@@ -1,7 +1,7 @@
 package com.saltpgp.notionproxy.controller;
 
+import com.saltpgp.notionproxy.dtos.ConsultantDto;
 import com.saltpgp.notionproxy.exceptions.NotionException;
-import com.saltpgp.notionproxy.models.Consultant;
 import com.saltpgp.notionproxy.service.NotionService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,9 +21,9 @@ public class NotionController {
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<List<String>> getNotion(@PathVariable UUID id) {
+    public ResponseEntity<ConsultantDto> getNotion(@PathVariable UUID id) {
         try {
-            return ResponseEntity.ok(notionService.getResponsiblePersonNameByUserId(id));
+            return ResponseEntity.ok(ConsultantDto.fromModel(notionService.getResponsiblePersonNameByUserId(id)));
         }
         catch (NotionException e) {
             return ResponseEntity.internalServerError().build();
@@ -31,9 +31,9 @@ public class NotionController {
     }
 
     @GetMapping()
-    public ResponseEntity<List<Consultant>> getConsultants() {
+    public ResponseEntity<List<ConsultantDto>> getConsultants() {
         try {
-            return ResponseEntity.ok(notionService.getConsultants());
+            return ResponseEntity.ok(notionService.getConsultants().stream().map(ConsultantDto::fromModel).toList());
         } catch (NotionException e) {
             return ResponseEntity.internalServerError().build();
         }
