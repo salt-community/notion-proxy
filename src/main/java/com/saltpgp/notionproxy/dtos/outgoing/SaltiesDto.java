e29355c70c75a4892754226def15321d84da9c1f
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-09-13 11:28:40 +0200

    Add email, githubUrl, and githubImageUrl to SaltiesDto
    Co-authored-by: Emiliana <emilianalila@gmail.com>
    Co-authored-by: Kiran Malieakkal <kiranmalieakkal18@gmail.com>
    Co-authored-by: Liam Frosteman Neogard <liamfrostemanneogard@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/main/java/com/saltpgp/notionproxy/dtos/outgoing/SaltiesDto.java
+++ src/main/java/com/saltpgp/notionproxy/dtos/outgoing/SaltiesDto.java
@@ -6,12 +6,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-public record SaltiesDto(String name, UUID id) {
+public record SaltiesDto(String name, UUID id, String email, String githubUrl, String githubImageUrl) {
 
     public static List<SaltiesDto> fromModel(List<Developer> developers) {
         List<SaltiesDto> salties = new ArrayList<>();
         for (Developer developer : developers) {
-            salties.add(new SaltiesDto(developer.getName(), developer.getId()));
+            salties.add(new SaltiesDto(
+                    developer.getName(),
+                    developer.getId(),
+                    developer.getEmail(),
+                    developer.getGithubUrl(),
+                    developer.getGithubImageUrl()));
         }
         return salties;
     }
