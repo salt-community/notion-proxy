6e5a4165101ceb3899cc094b8ad926fe95f8260b
Emilianalila <emilianalila@gmail.com>
2024-09-16 10:21:04 +0200

    Fix feching developer info bug
    Co-authored-by: Emiliana <emilianalila@gmail.com>
    Co-authored-by: Kiran Malieakkal <kiranmalieakkal18@gmail.com>
    Co-authored-by: Liam Frosteman Neogard <liamfrostemanneogard@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/main/java/com/saltpgp/notionproxy/dtos/outgoing/DeveloperDto.java
+++ src/main/java/com/saltpgp/notionproxy/dtos/outgoing/DeveloperDto.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.UUID;
 
 public record DeveloperDto(String name, UUID id, String githubUrl, String email, List<ScoreDto>scores) {
+
     public static DeveloperDto fromModel(Developer developer) {
         return new DeveloperDto(
                 developer.getName(),
