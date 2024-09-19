51f0f935a040afb2680025bde2fc6acf624fad98
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-09-19 11:35:24 +0200

    Add email field
    Co-authored-by: Emiliana <emilianalila@gmail.com>
    Co-authored-by: Kiran Malieakkal <kiranmalieakkal18@gmail.com>
    Co-authored-by: Liam Frosteman Neogard <liamfrostemanneogard@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/main/java/com/saltpgp/notionproxy/models/ResponsiblePerson.java
+++ src/main/java/com/saltpgp/notionproxy/models/ResponsiblePerson.java
@@ -2,5 +2,5 @@ package com.saltpgp.notionproxy.models;
 
 import java.util.UUID;
 
-public record ResponsiblePerson(String name, UUID id ) {
+public record ResponsiblePerson(String name, UUID id, String email) {
 }
