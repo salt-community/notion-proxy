a0f0dec97726142719c035805cc9de0fc1d2af78
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-10-04 10:50:03 +0200

    Remove unused import
    Co-authored-by: Emiliana <emilianalila@gmail.com>
    Co-authored-by: Kiran Malieakkal <kiranmalieakkal18@gmail.com>
    Co-authored-by: Liam Frosteman Neogard <liamfrostemanneogard@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/test/java/com/saltpgp/notionproxy/service/NotionProxyServiceTest.java
+++ src/test/java/com/saltpgp/notionproxy/service/NotionProxyServiceTest.java
@@ -1,7 +1,6 @@
 package com.saltpgp.notionproxy.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.saltpgp.notionproxy.exceptions.NotionException;
 import com.saltpgp.notionproxy.models.Consultant;
