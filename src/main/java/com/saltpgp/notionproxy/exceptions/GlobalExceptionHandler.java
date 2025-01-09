4083865fb3abd385def136f519a4e5cc3ce33ff1
Svetlana Soboleva <132843403+svetlana-soboleva@users.noreply.github.com>
2025-01-09 13:47:09 +0100

    start with global exceptions

---
+++ src/main/java/com/saltpgp/notionproxy/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.saltpgp.notionproxy.exceptions;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestController;
+
+@ControllerAdvice
+@RestController
+public class GlobalExceptionHandler {
+}
