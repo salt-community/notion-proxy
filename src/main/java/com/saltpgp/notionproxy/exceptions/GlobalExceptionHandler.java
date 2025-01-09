c7a1f3f35522905e79b7f894b0c5b9249eb07302
Svetlana Soboleva <132843403+svetlana-soboleva@users.noreply.github.com>
2025-01-09 14:03:22 +0100

    add NotionNotFoundException

--- src/main/java/com/saltpgp/notionproxy/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/saltpgp/notionproxy/exceptions/GlobalExceptionHandler.java
@@ -1,9 +1,17 @@
 package com.saltpgp.notionproxy.exceptions;
 
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
-@RestController
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    /***
+     * Handle NotionNotFoundException - Returns 404 NOT FOUND
+     */
+    @ExceptionHandler({NotionNotFoundException.class})
+    public ResponseEntity <Void> handleNotionNotFoundException(){return ResponseEntity.notFound().build();}
+
+
 }
