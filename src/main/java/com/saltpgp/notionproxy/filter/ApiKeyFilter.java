c78ba7b90f5baf15750af177f2abecbd06b07e8e
TobiasBlankJohansson <tobias@annito.se>
2025-01-10 13:07:39 +0100

    feat:add log info when Successful API access and log warn when Unauthorized access attempt

--- src/main/java/com/saltpgp/notionproxy/filter/ApiKeyFilter.java
+++ src/main/java/com/saltpgp/notionproxy/filter/ApiKeyFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -11,6 +12,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Component
+@Slf4j
 public class ApiKeyFilter extends OncePerRequestFilter {
 
     private final static String API_KEY_HEADER = "X-API-KEY";
@@ -22,8 +24,12 @@ public class ApiKeyFilter extends OncePerRequestFilter {
             throws IOException, ServletException {
         String requestApiKey = request.getHeader(API_KEY_HEADER);
         if (CUSTOM_API_KEY.equals(requestApiKey)) {
+            log.info("Successful API access: IP={}, Method={}, Path={}",
+                    request.getRemoteAddr(), request.getMethod(), request.getRequestURI());
             filterChain.doFilter(request, response);
         } else {
+            log.warn("Unauthorized access attempt: IP={}, Method={}, Path={}, API Key Valid={}",
+                    request.getRemoteAddr(), request.getMethod(), request.getRequestURI(), false);
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             response.getWriter().write("Unauthorized: Invalid API Key");
         }
