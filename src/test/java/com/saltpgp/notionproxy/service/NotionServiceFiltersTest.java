e3f01cd19c82dcac076fa0c15ac12f9282981f39
TobiasBlankJohansson <tobias@annito.se>
2025-01-15 09:15:45 +0100

    test:add tests for NotionServiceFiltersTest

--- src/test/java/com/saltpgp/notionproxy/service/NotionServiceFiltersTest.java
+++ src/test/java/com/saltpgp/notionproxy/service/NotionServiceFiltersTest.java
@@ -34,4 +34,77 @@ public class NotionServiceFiltersTest {
                 """;
         assertEquals(expected, NotionServiceFilters.FILTER_ON_ASSIGNMENT);
     }
+
+
+    @Test
+    public void testGetFilterOnAssignmentWithNullCursor() {
+        String expected = NotionServiceFilters.FILTER_ON_ASSIGNMENT;
+        assertEquals(expected, NotionServiceFilters.getFilterOnAssignment(null));
+    }
+
+    @Test
+    public void testGetFilterOnAssignmentWithCursor() {
+        String cursor = "abc123";
+        String expected = String.format("""
+                {
+                    \"start_cursor\": \"%s\"
+                    \"filter\": {
+                        \"property\": \"Status\",
+                        \"select\": {
+                            \"equals\": \"On Assignment\"
+                        }
+                    }
+                }
+                """, cursor);
+        assertEquals(expected, NotionServiceFilters.getFilterOnAssignment(cursor));
+    }
+
+    @Test
+    public void testGetFilterDeveloperWithNullCursorAndNullFilter() {
+        String expected = "{}";
+        assertEquals(expected, NotionServiceFilters.getFilterDeveloper(null, null));
+    }
+
+    @Test
+    public void testGetFilterDeveloperWithCursorAndNullFilter() {
+        String cursor = "abc123";
+        String expected = String.format("""
+                {\"start_cursor\": \"%s\"}
+                """, cursor);
+        assertEquals(expected, NotionServiceFilters.getFilterDeveloper(cursor, null));
+    }
+
+    @Test
+    public void testGetFilterDeveloperWithCursorAndFilter() {
+        String cursor = "abc123";
+        String filter = "In Progress";
+        String expected = String.format("""
+                {
+                    \"start_cursor\": \"%s\"
+                    \"filter\": {
+                        \"property\": \"Status\",
+                        \"select\": {
+                            \"equals\": \"%s\"
+                        }
+                    }
+                }
+                """, cursor, filter);
+        assertEquals(expected, NotionServiceFilters.getFilterDeveloper(cursor, filter));
+    }
+
+    @Test
+    public void testGetFilterDeveloperWithNullCursorAndFilter() {
+        String filter = "Completed";
+        String expected = String.format("""
+                {
+                    \"filter\": {
+                        \"property\": \"Status\",
+                        \"select\": {
+                            \"equals\": \"%s\"
+                        }
+                    }
+                }
+                """, filter);
+        assertEquals(expected, NotionServiceFilters.getFilterDeveloper(null, filter));
+    }
 }
