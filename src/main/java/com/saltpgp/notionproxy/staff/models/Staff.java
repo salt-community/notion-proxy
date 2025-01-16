380b3678d51491546aabb769a5f9577fa3b75d40
TibbersEvolved <erik.olin.spanga@gmail.com>
2025-01-16 13:41:53 +0100

    Staff endpoint with filter created

--- src/main/java/com/saltpgp/notionproxy/staff/models/Staff.java
+++ src/main/java/com/saltpgp/notionproxy/staff/models/Staff.java
@@ -1,4 +1,15 @@
 package com.saltpgp.notionproxy.staff.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Staff {
+    private String name, email;
+    private UUID id;
 }
