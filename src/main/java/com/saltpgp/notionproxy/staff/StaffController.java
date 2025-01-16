829004fa9793249db277208f587ad140ca9d5749
TibbersEvolved <erik.olin.spanga@gmail.com>
2025-01-16 11:18:55 +0100

    More boilerplating + added endpoints for staff layer

--- src/main/java/com/saltpgp/notionproxy/staff/StaffController.java
+++ src/main/java/com/saltpgp/notionproxy/staff/StaffController.java
@@ -1,4 +1,22 @@
 package com.saltpgp.notionproxy.staff;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api/staff")
+@CrossOrigin
+@Slf4j
 public class StaffController {
+
+    @GetMapping("")
+    public void getAllStaff() {
+    }
+
+    @GetMapping("/{id}")
+    public void getStaffById(@PathVariable UUID id) {
+
+    }
 }
