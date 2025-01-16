c7c4526a38f0869033c7ea0ef0fcf1b8e216e4c5
TobiasBlankJohansson <Tobias@annito.se>
2025-01-16 11:39:55 +0100

    feat:add getAllDevelopers in DeveloperService

--- src/main/java/com/saltpgp/notionproxy/developer/model/Developer.java
+++ src/main/java/com/saltpgp/notionproxy/developer/model/Developer.java
@@ -1,12 +1,9 @@
 package com.saltpgp.notionproxy.developer.model;
 
-import com.saltpgp.notionproxy.models.ResponsiblePerson;
-import com.saltpgp.notionproxy.models.Score;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
 import java.util.UUID;
 
 @Getter
@@ -21,7 +18,4 @@ public class Developer {
     private String email;
     private String status;
     private String totalScore;
-    private List<Score> scores;
-    private List<ResponsiblePerson> responsiblePersonList;
-
 }
--- src/main/java/com/saltpgp/notionproxy/developer/service/DeveloperService.java
+++ src/main/java/com/saltpgp/notionproxy/developer/service/DeveloperService.java
@@ -1,9 +1,18 @@
 package com.saltpgp.notionproxy.developer.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.saltpgp.notionproxy.developer.model.Developer;
+import com.saltpgp.notionproxy.exceptions.NotionException;
 import com.saltpgp.notionproxy.service.NotionApiService;
+import com.saltpgp.notionproxy.service.NotionServiceFilters;
+import com.saltpgp.notionproxy.service.NotionServiceUtility;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @Slf4j
@@ -22,6 +31,42 @@ public class DeveloperService {
         this.DATABASE_ID = DATABASE_ID;
         this.SCORE_DATABASE_ID = SCORE_DATABASE_ID;
         this.CORE_DATABASE_ID = CORE_DATABASE_ID;
+    }
 
+    public List<Developer> getAllDevelopers(String filter) throws NotionException {
+        List<Developer> allDevelopers = new ArrayList<>();
+        String nextCursor = null;
+        boolean hasMore = true;
+        while (hasMore) {
+            JsonNode response = notionApiService.fetchDatabase(DATABASE_ID, NotionServiceFilters.getFilterDeveloper(nextCursor, filter));
+
+            response.get("results").elements().forEachRemaining(element -> {
+                JsonNode properties = element.get("properties");
+                if (properties.get("Name").get("title").get(0) == null) return;
+                String githubUrl = element.get("properties").get("GitHub").get("url").asText().equals("null") ? null
+                        : properties.get("GitHub").get("url").asText();
+
+                String githubImageUrl = githubUrl == null ? null : githubUrl + ".png";
+                String status = NotionServiceUtility.getDeveloperStatus(element);
+                String totalScore = NotionServiceUtility.getDeveloperTotalScore(element);
+                String email = properties.get("Private Email").get("email").asText().equals("null") ? null
+                        : properties.get("Private Email").get("email").asText();
+
+                Developer developer = new Developer(
+                        properties.get("Name").get("title").get(0).get("plain_text").asText(),
+                        UUID.fromString(element.get("id").asText()),
+                        githubUrl,
+                        githubImageUrl,
+                        email,
+                        status,
+                        totalScore);
+
+                allDevelopers.add(developer);
+            });
+
+            nextCursor = response.get("next_cursor").asText();
+            hasMore = response.get("has_more").asBoolean();
+        }
+        return allDevelopers;
     }
 }
