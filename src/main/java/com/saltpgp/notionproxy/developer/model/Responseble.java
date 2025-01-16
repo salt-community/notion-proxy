990ee713c09df8bbba3d1f543977bf32058745a5
TobiasBlankJohansson <Tobias@annito.se>
2025-01-16 14:46:48 +0100

    feat:add responseble model

---
+++ src/main/java/com/saltpgp/notionproxy/developer/model/Responseble.java
@@ -0,0 +1,16 @@
+package com.saltpgp.notionproxy.developer.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Responseble {
+    private String name;
+    private UUID id;
+    private String email;
+}
--- src/main/java/com/saltpgp/notionproxy/developer/service/DeveloperService.java
+++ src/main/java/com/saltpgp/notionproxy/developer/service/DeveloperService.java
@@ -71,6 +71,4 @@ public class DeveloperService {
                 getDeveloperTotalScore(properties));
     }
 
-
-
 }
