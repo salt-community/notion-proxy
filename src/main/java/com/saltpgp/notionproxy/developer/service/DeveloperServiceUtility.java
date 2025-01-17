21f0c63e24ac0e5deee2bfcfb0452360a8088969
TobiasBlankJohansson <tobias@annito.se>
2025-01-17 07:50:23 +0100

    feat:add DeveloperNotionProperty

--- src/main/java/com/saltpgp/notionproxy/developer/service/DeveloperServiceUtility.java
+++ src/main/java/com/saltpgp/notionproxy/developer/service/DeveloperServiceUtility.java
@@ -2,6 +2,7 @@ package com.saltpgp.notionproxy.developer.service;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.saltpgp.notionproxy.developer.model.Responsible;
+import static com.saltpgp.notionproxy.developer.service.DeveloperNotionProperty.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,52 +12,12 @@ class DeveloperServiceUtility {
 
     private static final String STATUS_NONE = "none";
 
-    private static final class Properties {
-        private static final String STATUS = "Status";
-        private static final String TOTAL_SCORE = "Total Score";
-        private static final String ID = "id";
-        private static final String NAME = "Name";
-        private static final String GITHUB = "GitHub";
-        private static final String PRIVATE_EMAIL = "Private Email";
-        private static final String RESPONSIBLE = "Responsible";
-    }
-
-    private static final class Status {
-        private static final String SELECT = "select";
-        private static final String NAME_KEY = "name";
-    }
-
-    private static final class TotalScore {
-        private static final String FORMULA = "formula";
-        private static final String NUMBER = "number";
-    }
-
-    private static final class Name {
-        private static final String TITLE = "title";
-        private static final String PLAIN_TEXT = "plain_text";
-    }
-
-    private static final class GitHub {
-        private static final String URL = "url";
-    }
-
-    private static final class PrivateEmail {
-        private static final String EMAIL = "email";
-    }
-
-    private static final class NotionResponsible {
-        private static final String PEOPLE = "people";
-        private static final String PERSON = "person";
-        private static final String EMAIL = "email";
-    }
-
     private static final String NULL = "null";
     private static final String PNG = ".png";
 
     public static String getDeveloperStatus(JsonNode properties) {
         try {
-            return properties.get(Properties.STATUS).get(Status.SELECT)
-                    .get(Status.NAME_KEY).asText();
+            return properties.get(Properties.STATUS).get(Status.SELECT).get(Select.NAME_KEY).asText();
         } catch (Exception e) {
             return STATUS_NONE;
         }
@@ -65,7 +26,7 @@ class DeveloperServiceUtility {
     public static String getDeveloperTotalScore(JsonNode properties) {
         try {
             return String.valueOf(properties.get(Properties.TOTAL_SCORE).get(TotalScore.FORMULA)
-                    .get(TotalScore.NUMBER).asInt());
+                    .get(Formula.NUMBER).asInt());
         } catch (Exception e) {
             return STATUS_NONE;
         }
@@ -76,7 +37,7 @@ class DeveloperServiceUtility {
     }
 
     public static String getDeveloperName(JsonNode properties) {
-        return properties.get(Properties.NAME).get(Name.TITLE).get(0).get(Name.PLAIN_TEXT).asText();
+        return properties.get(Properties.NAME).get(Name.TITLE).get(0).get(Title.PLAIN_TEXT).asText();
     }
 
     public static String getDeveloperGithubUrl(JsonNode properties) {
@@ -115,6 +76,6 @@ class DeveloperServiceUtility {
     }
 
     private static String getResponsibleName(JsonNode responsible) throws Exception {
-        return responsible.get(Status.NAME_KEY).asText();
+        return responsible.get(Select.NAME_KEY).asText();
     }
 }
