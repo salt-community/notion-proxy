6681f49b74fbdeeb92c1b8a4a25c4d07c3d3c0a3
Lawe02 <89772680+Lawe02@users.noreply.github.com>
2025-01-18 13:08:48 +0100

    added model assignment

---
+++ src/main/java/com/saltpgp/notionproxy/assignment/model/Assignment.java
@@ -0,0 +1,18 @@
+package com.saltpgp.notionproxy.assignment.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+
+public class Assignment {
+    private String name;
+    private int score;
+    private List<String> categories;
+    private String comment;
+}
