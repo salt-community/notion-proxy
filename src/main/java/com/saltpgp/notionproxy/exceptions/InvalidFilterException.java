2b8e198be757f51ec76d106f72f6616dc8b653b4
Lawe02 <89772680+Lawe02@users.noreply.github.com>
2025-01-21 15:00:09 +0100

    added exception class for handeling invalid filter values

---
+++ src/main/java/com/saltpgp/notionproxy/exceptions/InvalidFilterException.java
@@ -0,0 +1,7 @@
+package com.saltpgp.notionproxy.exceptions;
+
+public class InvalidFilterException extends RuntimeException {
+  public InvalidFilterException(String message) {
+    super(message);
+  }
+}
