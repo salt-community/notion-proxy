888459cf73188237c29f1ab6509c4af0a6b4f499
TobiasBlankJohansson <tobias@annito.se>
2025-01-21 09:20:36 +0100

    refa:remove unneeded files

--- src/main/java/com/saltpgp/notionproxy/dtos/outgoing/ListScoreDto.java
+++
@@ -1,9 +0,0 @@
-package com.saltpgp.notionproxy.dtos.outgoing;
-
-import com.saltpgp.notionproxy.assignment.model.Assignment;
-
-import java.util.List;
-import java.util.UUID;
-
-public record ListScoreDto(UUID id, List<Assignment> scores) {
-}
