f557f6856f9fe24e474f9593b1fc3738dd7aa7c7
TobiasBlankJohansson <tobias@annito.se>
2025-01-21 13:35:33 +0100

    refa:refactor NotionApiService to use static fields for magic strings

--- src/main/java/com/saltpgp/notionproxy/service/NotionApiService.java
+++ src/main/java/com/saltpgp/notionproxy/service/NotionApiService.java
@@ -12,6 +12,7 @@ import org.springframework.web.client.RestClient;
 @Service
 public class NotionApiService {
 
+    public static final String NOTION_URL = "https://api.notion.com/v1";
     private final RestClient restClient;
     private final String API_KEY;
     private final String NOTION_VERSION;
@@ -19,34 +20,34 @@ public class NotionApiService {
     public NotionApiService(RestClient.Builder builder,
                             @Value("${NOTION_API_KEY}") String API_KEY,
                             @Value("${NOTION_VERSION}") String NOTION_VERSION) {
-        this.restClient = builder.baseUrl("https://api.notion.com/v1").build();
+        this.restClient = builder.baseUrl(NOTION_URL).build();
         this.API_KEY = API_KEY;
         this.NOTION_VERSION = NOTION_VERSION;
     }
 
     public JsonNode fetchPage(String pageId) throws NotionException {
-        String uri = String.format("/pages/%s", pageId);
+        String uri = String.format(UriNotionFormat.PAGES, pageId);
         return executeRequest(() -> restClient
                 .get()
                 .uri(uri)
-                .header("Content-Type", "application/json")
-                .header("Authorization", "Bearer " + API_KEY)
-                .header("Notion-Version", NOTION_VERSION)
+                .header(RequestHeader.CONTENT_TYPE, HeaderValue.CONTENT_TYPE)
+                .header(RequestHeader.AUTHORIZATION, HeaderValue.BEARER + API_KEY)
+                .header(RequestHeader.NOTION_VERSION, NOTION_VERSION)
                 .retrieve()
-                .body(JsonNode.class), pageId, "page");
+                .body(JsonNode.class), pageId, NotionType.PAGE);
     }
 
     public JsonNode fetchDatabase(String database, Object node) throws NotionException {
-        String uri = String.format("/databases/%s/query", database);
+        String uri = String.format(UriNotionFormat.DATABASES, database);
         return executeRequest(() -> restClient
                 .post()
                 .uri(uri)
-                .header("Content-Type", "application/json")
-                .header("Authorization", "Bearer " + API_KEY)
-                .header("Notion-Version", NOTION_VERSION)
+                .header(RequestHeader.CONTENT_TYPE, HeaderValue.CONTENT_TYPE)
+                .header(RequestHeader.AUTHORIZATION, HeaderValue.BEARER + API_KEY)
+                .header(RequestHeader.NOTION_VERSION, NOTION_VERSION)
                 .body(node)
                 .retrieve()
-                .body(JsonNode.class), database, "database");
+                .body(JsonNode.class), database, NotionType.DATABASE);
     }
 
     private JsonNode executeRequest(RequestExecutor executor, String id, String type) throws NotionException {
@@ -54,26 +55,62 @@ public class NotionApiService {
             return executor.execute();
         } catch (HttpClientErrorException e) {
             switch (e.getStatusText()) {
-                case "Not Found":
-                    if ("page".equals(type)) {
-                        throw new NotionException("Page ID didn't exist in Notion: " + id);
-                    } else if ("database".equals(type)) {
-                        throw new NotionException("Database didn't exist in Notion");
+                case HttpClientCase.NOT_FOUND:
+                    if (NotionType.PAGE.equals(type)) {
+                        throw new NotionException(ErrorNotionMessage.PAGE_ID_DIDNT_EXIST + id);
+                    }
+                    if (NotionType.DATABASE.equals(type)) {
+                        throw new NotionException(ErrorNotionMessage.DATABASE_DIDNT_EXIST);
                     }
                     break;
-                case "Unauthorized":
-                    throw new NotionException("Unauthorized to access Notion API. Check the API key.");
-                case "Bad Request":
-                    throw new NotionException("Bad request to the Notion API. Check the API request.");
+                case HttpClientCase.UNAUTHORIZED:
+                    throw new NotionException(ErrorNotionMessage.UNAUTHORIZED_TO_ACCESS);
+                case HttpClientCase.BAD_REQUEST:
+                    throw new NotionException(ErrorNotionMessage.BAD_REQUEST);
                 default:
-                    throw new NotionException("Unknown error occurred with HTTP status: " + e.getStatusText());
+                    throw new NotionException(ErrorNotionMessage.UNKNOWN_ERROR_WITH_HTTP_STATUS + e.getStatusText());
             }
         } catch (ResourceAccessException e) {
-            throw new NotionException("Can't access Notion API. Check if the Notion proxy can send requests.");
+            throw new NotionException(ErrorNotionMessage.CANT_ACCESS);
         } catch (Exception e) {
-            throw new NotionException("Unknown error occurred while trying to send request to Notion.");
+            throw new NotionException(ErrorNotionMessage.UNKNOWN_ERROR_WHEN_SEND_REQUEST);
         }
         return null;
+    }
+
+    public static class UriNotionFormat{
+        public static final String PAGES = "/pages/%s";
+        public static final String DATABASES = "/databases/%s/query";
+    }
+
+    public static class HeaderValue{
+        public static final String CONTENT_TYPE = "application/json";
+        public static final String BEARER = "Bearer ";
+    }
+
+    public static class RequestHeader{
+        public static final String CONTENT_TYPE = "Content-Type";
+        public static final String AUTHORIZATION = "Authorization";
+        public static final String NOTION_VERSION = "Notion-Version";
+    }
+
+    public static class HttpClientCase {
+        private static final String UNAUTHORIZED = "Unauthorized";
+        private static final String BAD_REQUEST = "Bad Request";
+        private static final String NOT_FOUND = "Not Found";
+    }
+    public static class ErrorNotionMessage{
+        private static final String UNAUTHORIZED_TO_ACCESS = "Unauthorized to access Notion API. Check the API key.";
+        private static final String BAD_REQUEST = "Bad request to the Notion API. Check the API request.";
+        private static final String UNKNOWN_ERROR_WITH_HTTP_STATUS = "Unknown error occurred with HTTP status: ";
+        private static final String CANT_ACCESS = "Can't access Notion API. Check if the Notion proxy can send requests.";
+        private static final String UNKNOWN_ERROR_WHEN_SEND_REQUEST = "Unknown error occurred while trying to send request to Notion.";
+        private static final String PAGE_ID_DIDNT_EXIST = "Page ID didn't exist in Notion: ";
+        private static final String DATABASE_DIDNT_EXIST = "Database didn't exist in Notion";
+    }
+    public static class NotionType{
+        private static final String PAGE = "page";
+        private static final String DATABASE = "database";
     }
 
     @FunctionalInterface
