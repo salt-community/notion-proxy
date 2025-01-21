40c878f3ffb81968e9681bf3b576e62e86fe8585
TobiasBlankJohansson <tobias@annito.se>
2025-01-21 14:48:05 +0100

    feat:add getAssignmentsByUserId returns AssignmentDto

--- src/main/java/com/saltpgp/notionproxy/assignment/controller/AssignmentController.java
+++ src/main/java/com/saltpgp/notionproxy/assignment/controller/AssignmentController.java
@@ -1,5 +1,6 @@
 package com.saltpgp.notionproxy.assignment.controller;
 
+import com.saltpgp.notionproxy.assignment.controller.dtos.AssignmentDto;
 import com.saltpgp.notionproxy.assignment.model.Assignment;
 import com.saltpgp.notionproxy.assignment.service.AssignmentService;
 import com.saltpgp.notionproxy.exceptions.NotionException;
@@ -20,9 +21,9 @@ public class AssignmentController {
     }
 
     @GetMapping()
-    public ResponseEntity<List<Assignment>> getAssignmentsByUserId(@RequestParam(value = "developerId", required = true) UUID developerId) throws NotionException {
+    public ResponseEntity<AssignmentDto> getAssignmentsByUserId(@RequestParam(value = "developerId", required = true) UUID developerId) throws NotionException {
         List<Assignment> assignments = assignmentService.getAssignmentsFromDeveloper(developerId);
-        return ResponseEntity.ok(assignments);
+        return ResponseEntity.ok(new AssignmentDto(developerId.toString(), assignments));
     }
 
     @GetMapping("/{developerId}")
