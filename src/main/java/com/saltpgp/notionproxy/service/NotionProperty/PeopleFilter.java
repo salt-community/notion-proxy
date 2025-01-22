4e14a75d1129be947a9984c9d83f416720fa7a7c
TibbersEvolved <salt-dev@MacBook-Pro-10.local>
2025-01-22 09:17:40 +0100

    Added filtertypes for People and Multi-select

--- src/main/java/com/saltpgp/notionproxy/service/NotionProperty/PeopleFilter.java
+++ src/main/java/com/saltpgp/notionproxy/service/NotionProperty/PeopleFilter.java
@@ -1,14 +1,14 @@
 package com.saltpgp.notionproxy.service.NotionProperty;
 
-public enum SelectFilter {
-    EQUALS ("equals"),
-    DOES_NOT_EQUALS ("does_not_equal"),
+public enum PeopleFilter {
+    CONTAINS ("contains"),
+    DOES_NOT_CONTAIN ("does_not_contain"),
     IS_EMPTY ("is_empty"),
     IS_NOT_EMPTY ("is_not_empty");
 
     private final String value;
 
-    private SelectFilter(String s) {
+    private PeopleFilter(String s) {
         value = s;
     }
 
