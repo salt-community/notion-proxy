ab6ef16883304c3f57c3370b985709fed6e98960
TobiasBlankJohansson <Tobias@annito.se>
2025-01-23 09:01:12 +0100

    feat:add spring-cloud-gcp-starter-storage<

--- src/main/java/com/saltpgp/notionproxy/bucket/bucket.java
+++ src/main/java/com/saltpgp/notionproxy/bucket/bucket.java
@@ -1,29 +1,20 @@
 package com.saltpgp.notionproxy.bucket;
 
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestClient;
 
 @Component
 public class bucket {
-    private final RestClient restClient;
-    private final String ENDPOINT;
-    private final String ACCESS_KEY;
-    private final String SECRET_KEY;
-    private final String BUCKET_NAME;
 
-    public bucket(RestClient.Builder builder,
-                  @Value("${BUCKET_ENDPOINT}") String ENDPOINT,
-                  @Value("${BUCKET_ACCESS_KEY}") String ACCESS_KEY,
-                  @Value("${BUCKET_SECRET_KEY}") String SECRET_KEY,
-                  @Value("${BUCKET_BUCKET_NAME}") String BUCKET_NAME) {
-        this.restClient = builder.baseUrl(ENDPOINT).build();
-        this.ENDPOINT = ENDPOINT;
-        this.ACCESS_KEY = ACCESS_KEY;
-        this.SECRET_KEY = SECRET_KEY;
-        this.BUCKET_NAME = BUCKET_NAME;
+    private final Storage storageClient;
+
+    public bucket(
+            storageClient = StorageOptions.newBuilder()
+        .setHost(fakeGcsExternalUrl)
+        .setProjectId("test-project")
+        .setCredentials(NoCredentials.getInstance())
+            .build()
+        .getService();
     }
 
     public void stuff(){
