9957511ac119b69126553a4d007f023aa901366b
TobiasBlankJohansson <Tobias@annito.se>
2025-01-23 16:17:54 +0100

    refa:refactor to return jsonNode

--- src/main/java/com/saltpgp/notionproxy/assignment/model/Assignment.java
+++ src/main/java/com/saltpgp/notionproxy/assignment/model/Assignment.java
@@ -2,6 +2,7 @@ package com.saltpgp.notionproxy.assignment.model;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.AllArgsConstructor;
@@ -23,16 +24,16 @@ public class Assignment {
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    public static String toJson(Assignment assignment) throws JsonProcessingException {
-        return objectMapper.writeValueAsString(assignment);
+    public static JsonNode toJsonNode(Assignment assignment) {
+        return objectMapper.convertValue(assignment, JsonNode.class);
+    }
+
+    public static JsonNode toJsonNode(List<Assignment> assignmentList) {
+        return objectMapper.convertValue(assignmentList, JsonNode.class);
     }
 
     public static Assignment fromJson(String json) throws JsonProcessingException {
         return objectMapper.readValue(json, Assignment.class);
-    }
-
-    public static String toJsonList(List<Assignment> assignmentList) throws JsonProcessingException {
-        return objectMapper.writeValueAsString(assignmentList);
     }
 
     public static List<Assignment> fromJsonList(String json) throws JsonProcessingException {
