a0a95d6268a4fd1df03bafa10fe6bfacbfcc3c9b
TobiasBlankJohansson <Tobias@annito.se>
2025-01-23 16:12:31 +0100

    feat:add from and to Json in Assignment

--- src/main/java/com/saltpgp/notionproxy/assignment/model/Assignment.java
+++ src/main/java/com/saltpgp/notionproxy/assignment/model/Assignment.java
@@ -1,5 +1,9 @@
 package com.saltpgp.notionproxy.assignment.model;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,4 +20,22 @@ public class Assignment {
     private int score;
     private List<String> categories;
     private String comment;
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static String toJson(Assignment assignment) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(assignment);
+    }
+
+    public static Assignment fromJson(String json) throws JsonProcessingException {
+        return objectMapper.readValue(json, Assignment.class);
+    }
+
+    public static String toJsonList(List<Assignment> assignmentList) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(assignmentList);
+    }
+
+    public static List<Assignment> fromJsonList(String json) throws JsonProcessingException {
+        return objectMapper.readValue(json, new TypeReference<List<Assignment>>() {});
+    }
 }
