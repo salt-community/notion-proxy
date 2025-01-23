353a37c6661cc73a13fe42985d264fa23935b237
TobiasBlankJohansson <Tobias@annito.se>
2025-01-23 09:24:21 +0100

    feat:add uploadFile methode in bucket

--- src/main/java/com/saltpgp/notionproxy/bucket/BucketApi.java
+++ src/main/java/com/saltpgp/notionproxy/bucket/BucketApi.java
@@ -2,9 +2,13 @@ package com.saltpgp.notionproxy.bucket;
 
 
 import com.google.cloud.NoCredentials;
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.Bucket;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class BucketApi {
@@ -12,6 +16,8 @@ public class BucketApi {
     private final Storage storageClient;
 
     private final String fakeGcsExternalUrl = "http://localhost:9000";
+
+    String bucketName = "my-local-bucket";
 
     public BucketApi() {
         storageClient = StorageOptions.newBuilder()
@@ -22,7 +28,13 @@ public class BucketApi {
                 .getService();
     }
 
-    public void stuff() {
+    public void uploadFile(String cacheName, Object cache) {
+        Bucket bucket = storageClient.get(cacheName);
+        if (bucket == null) {
+            throw new RuntimeException("Bucket not found: " + cacheName);
+        }
 
+        Blob blob = bucket.create(cacheName, cache.toString().getBytes(StandardCharsets.UTF_8));
+        System.out.println("File uploaded to " + blob.getMediaLink());
     }
 }
