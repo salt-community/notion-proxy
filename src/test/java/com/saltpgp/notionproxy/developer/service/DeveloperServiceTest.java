09ad13c5cc43993881aef3e9fd449d5957c2b9ee
John Eliasson <john.eliasson@appliedtechnology.se>
2025-01-23 08:39:51 +0100

    add test to get developer

--- src/test/java/com/saltpgp/notionproxy/developer/service/DeveloperServiceTest.java
+++ src/test/java/com/saltpgp/notionproxy/developer/service/DeveloperServiceTest.java
@@ -2,13 +2,19 @@ package com.saltpgp.notionproxy.developer.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.saltpgp.notionproxy.developer.model.Developer;
 import com.saltpgp.notionproxy.exceptions.NotionException;
+import com.saltpgp.notionproxy.exceptions.NotionNotFoundException;
 import com.saltpgp.notionproxy.notionapi.NotionApiService;
 import com.saltpgp.notionproxy.service.NotionServiceFilters;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 
 class DeveloperServiceTest {
 
@@ -29,6 +35,38 @@ class DeveloperServiceTest {
         developerService = new DeveloperService(mockApiService, DATABASE_ID);
 
         mapper = new ObjectMapper();
+
+
+            String pageResponse = """
+        {
+            "id": "11111111-1111-1111-1111-111111111111",
+            "properties": {
+                "Name": {
+                    "title": [
+                        {
+                            "plain_text": "Test Saltie 1"
+                        }
+                    ]
+                },
+                "GitHub": {
+                    "url": "https://github.com/saltie1"
+                },
+                "Private Email": {
+                    "email": "saltie@example.com"
+                },
+                "Responsible": {
+                    "people": [
+                        {
+                            "name": "Responsible Person 1",
+                            "person": {
+                                "email": "responsibleperson1@appliedtechnology.se"
+                            }
+                        }
+                    ]
+                }
+            }
+        }
+    """;
 
         databaseResponse = """
                 {
@@ -129,6 +167,25 @@ class DeveloperServiceTest {
                 """;
         //Fix getFilterOnAssignment to use the builder
         when(mockApiService.fetchDatabase(DATABASE_ID, NotionServiceFilters.getFilterOnAssignment(null))).thenReturn(mapper.readTree(databaseResponse));
+        when(mockApiService.fetchPage("11111111-1111-1111-1111-111111111111"))
+                .thenReturn(mapper.readTree(pageResponse));
+    }
+
+    @Test
+    void shouldGetDeveloperById() throws NotionException, NotionNotFoundException {
+
+        UUID developerId = UUID.fromString("11111111-1111-1111-1111-111111111111");
+
+        Developer developer = developerService.getDeveloperById(developerId);
+
+        assertNotNull(developer);
+        assertEquals("Test Saltie 1", developer.getName());
+        assertEquals(developerId, developer.getId());
+
+        assertEquals("https://github.com/saltie1", developer.getGithubUrl());
+        assertEquals("saltie@example.com", developer.getEmail());
+
+
 
     }
 }
