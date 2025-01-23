8b547c63eb7b54c53daa7fd7936a73c1b0550035
TobiasBlankJohansson <Tobias@annito.se>
2025-01-23 11:41:57 +0100

    feat:add timestamp to invalidate the cache if it's to old

--- src/main/java/com/saltpgp/notionproxy/bucket/BucketApi.java
+++ src/main/java/com/saltpgp/notionproxy/bucket/BucketApi.java
@@ -3,6 +3,7 @@ package com.saltpgp.notionproxy.bucket;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.cloud.NoCredentials;
 import com.google.cloud.storage.*;
 import org.apache.catalina.mapper.Mapper;
@@ -29,14 +30,16 @@ public class BucketApi {
                 .getService();
     }
 
-    public void uploadFile(String cacheName, Object cache) {
+    public void uploadFile(String cacheName, JsonNode jsonNode) {
+        ObjectNode objectNode = (ObjectNode) jsonNode;
+        objectNode.put("timestamp", System.currentTimeMillis());
         Bucket bucket = storageClient.get(bucketName);
         if (bucket == null) {
             bucket = storageClient.create(BucketInfo.of(bucketName));
             System.out.println("Bucket created: " + bucketName);
         }
 
-        Blob blob = bucket.create(cacheName, cache.toString().getBytes(StandardCharsets.UTF_8));
+        Blob blob = bucket.create(cacheName, objectNode.toString().getBytes(StandardCharsets.UTF_8));
         System.out.println("File uploaded to " + blob.getMediaLink());
     }
 
@@ -53,10 +56,19 @@ public class BucketApi {
             System.out.println("File not found: " + cacheName);
             return null;
         }
-
-        ObjectMapper mapper = new ObjectMapper();
-
         byte[] content = blob.getContent();
-        return mapper.readTree(content);
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = mapper.readTree(content);
+
+
+        try {
+            if (System.currentTimeMillis() - jsonNode.get("timestamp").asLong() > 60000) {
+                return null;
+            }
+        } catch (Exception e) {
+            return null;
+        }
+
+        return jsonNode;
     }
-    }
+}
