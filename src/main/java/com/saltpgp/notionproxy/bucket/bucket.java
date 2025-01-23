74e8da557e31b0e8fed61280eed89203ad3d8705
TobiasBlankJohansson <Tobias@annito.se>
2025-01-23 09:17:46 +0100

    feat:add storageClient in bucket

--- src/main/java/com/saltpgp/notionproxy/bucket/bucket.java
+++ src/main/java/com/saltpgp/notionproxy/bucket/bucket.java
@@ -1,6 +1,9 @@
 package com.saltpgp.notionproxy.bucket;
 
 
+import com.google.cloud.NoCredentials;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,16 +11,18 @@ public class bucket {
 
     private final Storage storageClient;
 
-    public bucket(
-            storageClient = StorageOptions.newBuilder()
-        .setHost(fakeGcsExternalUrl)
-        .setProjectId("test-project")
-        .setCredentials(NoCredentials.getInstance())
-            .build()
-        .getService();
+    private final String fakeGcsExternalUrl = "http://localhost:9000";
+
+    public bucket() {
+        storageClient = StorageOptions.newBuilder()
+                .setHost(fakeGcsExternalUrl)
+                .setProjectId("test-project")
+                .setCredentials(NoCredentials.getInstance())
+                .build()
+                .getService();
     }
 
-    public void stuff(){
+    public void stuff() {
 
     }
 }
