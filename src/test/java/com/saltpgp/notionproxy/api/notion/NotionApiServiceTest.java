5f8b3501455ec41d90f8937398b6608771b65f54
TobiasBlankJohansson <tobias@annito.se>
2025-01-29 10:45:50 +0100

    refa:refactor test organitation

--- src/test/java/com/saltpgp/notionproxy/api/notion/NotionApiServiceTest.java
+++ src/test/java/com/saltpgp/notionproxy/api/notion/NotionApiServiceTest.java
@@ -1,10 +1,9 @@
-package com.saltpgp.notionproxy.service;
+package com.saltpgp.notionproxy.api.notion;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.saltpgp.notionproxy.exceptions.NotionException;
-import com.saltpgp.notionproxy.api.notion.NotionApiService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
