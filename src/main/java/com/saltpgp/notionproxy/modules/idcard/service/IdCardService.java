34a9998fb993c5c780cfaf3184e80702238074f9
TobiasBlankJohansson <tobias@annito.se>
2025-02-21 14:17:30 +0100

    feat:add IdCardService with constructor

---
+++ src/main/java/com/saltpgp/notionproxy/modules/idcard/service/IdCardService.java
@@ -0,0 +1,30 @@
+package com.saltpgp.notionproxy.modules.idcard.service;
+
+import com.saltpgp.notionproxy.api.bucket.BucketApiService;
+import com.saltpgp.notionproxy.api.notion.NotionApiService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class IdCardService {
+    private final NotionApiService notionApiService;
+    private final BucketApiService bucketApiService;
+    private final String DATABASE_ID;
+
+    private static final String FILTER = """
+                "filter": {
+                    "property": "Status",
+                    "select": {
+                        "equals": "%s"
+                    }
+                }
+            """;
+
+    public IdCardService(NotionApiService notionApiService, BucketApiService bucketApiService, @Value("${DATABASE_ID}") String DATABASE_ID) {
+        this.notionApiService = notionApiService;
+        this.bucketApiService = bucketApiService;
+        this.DATABASE_ID = DATABASE_ID;
+    }
+}
