a62daa9a579db1826eec3639cfe533ee311f3801
TobiasBlankJohansson <tobias@annito.se>
2025-02-21 14:12:42 +0100

    feat:add user

---
+++ src/main/java/com/saltpgp/notionproxy/modules/idcard/model/User.java
@@ -0,0 +1,29 @@
+package com.saltpgp.notionproxy.modules.idcard.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+    private String name, course, endDate, status, image;
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static JsonNode toJsonNode(User user) {
+        return objectMapper.convertValue(user, JsonNode.class);
+    }
+
+    public static User fromJson(String json) throws JsonProcessingException {
+        return objectMapper.readValue(json, User.class);
+    }
+
+}
