a6e99ca7462bf683bcb5efc3be9957b834a3d7d1
TobiasBlankJohansson <tobias@annito.se>
2025-02-21 14:46:27 +0100

    feat:add cache in getIdCard

--- src/main/java/com/saltpgp/notionproxy/modules/idcard/service/IdCardService.java
+++ src/main/java/com/saltpgp/notionproxy/modules/idcard/service/IdCardService.java
@@ -6,11 +6,13 @@ import com.saltpgp.notionproxy.api.notion.NotionApiService;
 import com.saltpgp.notionproxy.exceptions.NotionException;
 import com.saltpgp.notionproxy.exceptions.NotionNotFoundException;
 import com.saltpgp.notionproxy.modules.idcard.model.User;
+import com.saltpgp.notionproxy.modules.staff.model.Staff;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import static com.saltpgp.notionproxy.api.notion.filter.NotionServiceFilters.filterBuilder;
+import static com.saltpgp.notionproxy.modules.staff.service.StaffProperty.CACHE_ID;
 
 @Service
 @Slf4j
@@ -34,15 +36,26 @@ public class IdCardService {
         this.DATABASE_ID = DATABASE_ID;
     }
 
-    public User getIdCard(String email) throws NotionException, NotionNotFoundException {
+    public User getIdCard(String email, boolean useCache) throws NotionException, NotionNotFoundException {
+        if (useCache) {
+            JsonNode cache = bucketApiService.getCache(CACHE_ID + email);
+            try {
+                return User.fromJson(cache.toString());
+            } catch (Exception e) {
+                log.warn("Failed to parse cached user for email: {}. Error: {}", email, e.getMessage());
+            }
+        }
+        log.debug("Fetching dev by email: {}", email);
         JsonNode response = notionApiService.fetchDatabase(DATABASE_ID, filterBuilder(null, email, FILTER));
         JsonNode properties = response.get("result").get(0).get("properties");
-        return new User(
+        User user = new User(
                 properties.get("Name").get("title").get("text").get("content").asText(),
                 properties.get("Course").get("select").get("name").asText(),
                 properties.get("Payroll End Date").get("date").asText(),
                 properties.get("Status").get("select").get("name").asText(),
                 properties.get("GitHub").get("url").asText()
-                );
+        );
+        bucketApiService.saveCache(CACHE_ID + email, User.toJsonNode(user));
+        return user;
     }
 }
