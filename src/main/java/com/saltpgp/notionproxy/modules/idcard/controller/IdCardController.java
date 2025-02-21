ee00613ecc96bbd29599c5adae3f9ca1912ad47c
TobiasBlankJohansson <tobias@annito.se>
2025-02-21 14:53:54 +0100

    feat:add getIdCard in controller

--- src/main/java/com/saltpgp/notionproxy/modules/idcard/controller/IdCardController.java
+++ src/main/java/com/saltpgp/notionproxy/modules/idcard/controller/IdCardController.java
@@ -1,10 +1,17 @@
 package com.saltpgp.notionproxy.modules.idcard.controller;
 
+import com.saltpgp.notionproxy.exceptions.NotionException;
+import com.saltpgp.notionproxy.exceptions.NotionNotFoundException;
+import com.saltpgp.notionproxy.modules.idcard.controller.dtos.UserDto;
 import com.saltpgp.notionproxy.modules.idcard.service.IdCardService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/id-cards")
@@ -17,6 +24,20 @@ public class IdCardController {
         this.idCardService = idCardService;
     }
 
-
+    @GetMapping("{email}")
+    @Operation(summary = "Get a specific user by email",
+            description = "Retrieve details of a user by their unique email.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved user"),
+            @ApiResponse(responseCode = "404", description = "Developer not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    public ResponseEntity<UserDto> getIdCard(
+            @PathVariable String email,
+            @RequestParam(value = "useCache", required = false, defaultValue = "true") boolean useCache)
+            throws NotionException, NotionNotFoundException {
+        log.info("Request received to get user with email: {}", email);
+        return ResponseEntity.ok(UserDto.fromModel(idCardService.getIdCard(email, useCache)));
+    }
 
 }
