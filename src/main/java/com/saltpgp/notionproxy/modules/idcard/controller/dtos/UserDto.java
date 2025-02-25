d9690cf1942b8392c26c2bfc53924462573ac41b
TobiasBlankJohansson <Tobias@annito.se>
2025-02-25 14:31:30 +0100

    feat:add update for UserDto

--- src/main/java/com/saltpgp/notionproxy/modules/idcard/controller/dtos/UserDto.java
+++ src/main/java/com/saltpgp/notionproxy/modules/idcard/controller/dtos/UserDto.java
@@ -2,13 +2,13 @@ package com.saltpgp.notionproxy.modules.idcard.controller.dtos;
 
 import com.saltpgp.notionproxy.modules.idcard.model.User;
 
-public record UserDto(String name, String course, String endDate, String status, String gitHub) {
+public record UserDto(String uuid, String name, String course, String email, String gitHub) {
     public static UserDto fromModel(User user) {
         return new UserDto(
+                user.getUuid(),
                 user.getName(),
                 user.getCourse(),
-                user.getEndDate(),
-                user.getStatus(),
+                user.getEmail(),
                 user.getGitHub()
         );
     }
